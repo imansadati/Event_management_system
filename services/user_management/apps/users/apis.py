6c31a694e9a21f236eebae6cdeb64cc7d5fe6580
iman sadati <iman.3adati@gmail.com>
2025-03-03 22:33:03 +0330

    feat: Add custom pagination to AdminUserList api.

--- services/user_management/apps/users/apis.py
+++ services/user_management/apps/users/apis.py
@@ -3,10 +3,14 @@ from rest_framework import serializers
 from .models import AdminUser
 from django.http import HttpRequest
 from .selectors import user_admin_list
-from rest_framework.response import Response
+# read ./shared_utils/README.md
+from shared_utils.pagination import LimitOffsetPagination, get_paginated_response
 
 
 class AdminUserListApi(APIView):
+    class Pagination(LimitOffsetPagination):
+        default_limit = 1
+
     class FilterSerializer(serializers.Serializer):
         email = serializers.EmailField(required=False)
         full_name = serializers.CharField(max_length=128, required=False)
@@ -15,6 +19,8 @@ class AdminUserListApi(APIView):
         username = serializers.CharField(max_length=128, required=False)
 
     class OutputAdminSerializer(serializers.ModelSerializer):
+        password = serializers.CharField(max_lenght=128)
+
         class Meta:
             model = AdminUser
             fields = '__all__'
@@ -25,5 +31,10 @@ class AdminUserListApi(APIView):
 
         users = user_admin_list(filters=filters_serializer.validated_data)
 
-        serializer = self.OutputAdminSerializer(users, many=True).data
-        return Response(serializer)
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OutputAdminSerializer,
+            queryset=users,
+            request=request,
+            view=self
+        )
