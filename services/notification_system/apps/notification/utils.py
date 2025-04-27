4e33f9de283314147ff61c27ef5bc7d1afe4d3af
iman sadati <iman.3adati@gmail.com>
2025-04-27 19:54:43 +0330

    style: add some print to utils to show in log.

--- services/notification_system/apps/notification/utils.py
+++ services/notification_system/apps/notification/utils.py
@@ -4,13 +4,14 @@ from .services import create_email_log, save_failed_emails_to_redis
 def send_email(recipient, subject, body):
     try:
         # Simulate email sending (Replace with actual SMTP call)
-        print(f"Sending email to {recipient}: {subject}")
 
         create_email_log(recipient=recipient, subject=subject,
                          body=body, status='sent')
+        print(f"Email sent: {recipient} - {subject}")
         return True
 
     except Exception as e:
+        print(f"[Error] Failed to send email: {str(e)}")
         create_email_log(recipient, subject, body,
                          status='failed', error=str(e))
         save_failed_emails_to_redis(
