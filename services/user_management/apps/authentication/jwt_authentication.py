31a463362578d03cce2cf57fc0e2ede27b566c08
iman sadati <iman.3adati@gmail.com>
2025-05-26 17:53:11 +0330

    fix: Resolve user identification.

--- services/user_management/apps/authentication/jwt_authentication.py
+++ services/user_management/apps/authentication/jwt_authentication.py
@@ -1,22 +1,28 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from apps.users.models import AttendeeUser, StaffUser, AdminUser
-from django.db.models import Q
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
+
+
+ROLE_MODEL_MAP = {
+    'attendee': AttendeeUser,
+    'admin': AdminUser,
+    'staff': StaffUser,
+}
 
 
 class CustomJWTAuthentication(JWTAuthentication):
     def get_user(self, validated_token):
         """Override get_user() to support multiple user models."""
+        role = validated_token.get('role')
         user_id = validated_token.get('user_id')
 
-        if not user_id:
+        model = ROLE_MODEL_MAP.get(role)
+        status = f'is_{role}'
+
+        if not model and not user_id:
             return None
 
-        user = (
-            AttendeeUser.objects.filter(Q(id=user_id) & Q(is_attendee=True)).first() or
-            AdminUser.objects.filter(Q(id=user_id) & Q(is_admin=True)).first() or
-            StaffUser.objects.filter(Q(id=user_id) & Q(is_staff=True)).first()
-        )
+        user = model.objects.filter(id=user_id, **{status: True}).first()
 
         if user:
             return user
