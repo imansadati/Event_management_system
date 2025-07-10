3939c0649489e06e34650c41c4de383cb8b154b1
iman sadati <iman.3adati@gmail.com>
2025-07-10 16:18:35 +0330

    feat: Create serializer and events list api.

--- services/event_management/apps/events/apis.py
+++ services/event_management/apps/events/apis.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
+from rest_framework import serializers
+from django.http import HttpRequest
+from .models import Event
+from rest_framework.response import Response
 
-# Create your views here.
+
+class EventListApi(APIView):
+    class OutputEventListSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Event
+            fields = '__all__'
+
+    def get(self, request: HttpRequest):
+        pass
