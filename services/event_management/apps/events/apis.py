2639bce2ae43b92832ee187ede88aaa8473c5b16
iman sadati <iman.3adati@gmail.com>
2025-07-10 17:13:03 +0330

    feat: Add custom pagination.

--- services/event_management/apps/events/apis.py
+++ services/event_management/apps/events/apis.py
@@ -3,20 +3,29 @@ from rest_framework.viewsets import ViewSet
 from rest_framework import serializers
 from django.http import HttpRequest
 from .models import Event
-from rest_framework.response import Response
 from .selectors import event_list
+from shared_utils.pagination import get_paginated_response, LimitOffsetPagination
 
 
 class EventListApi(APIView):
+    class Pagination(LimitOffsetPagination):
+        default_limit = 2
+
     class OutputEventListSerializer(serializers.ModelSerializer):
         class Meta:
             model = Event
             fields = '__all__'
 
     def get(self, request: HttpRequest):
-        data = event_list()
-        serializer = self.OutputEventListSerializer(data, many=True).data
-        return Response(serializer)
+        events = event_list()
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OutputEventListSerializer,
+            queryset=events,
+            request=request,
+            view=self
+        )
 
 
 class EventGetwayApiViewSet(ViewSet):
