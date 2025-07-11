0b67d481acf6e72ea637ffbe9379f2d688ac6ad0
iman sadati <iman.3adati@gmail.com>
2025-07-11 22:02:49 +0330

    feat: Add configs in celery_config.py

---
+++ services/event_management/core/celery_config.py
@@ -0,0 +1,17 @@
+from celery import Celery
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery('core')
+app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
+
+app.conf.task_acks_late = True
+app.conf.task_default_priority = 5
+
+app.conf.task_routes = {
+
+}
+
+app.autodiscover_tasks()
