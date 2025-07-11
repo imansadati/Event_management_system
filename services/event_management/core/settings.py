f9542f6b5e680477f91788fe1d1c80a01077af4a
iman sadati <iman.3adati@gmail.com>
2025-07-11 22:30:59 +0330

    fix: Add correct environment variables and fix celery config.

--- services/event_management/core/settings.py
+++ services/event_management/core/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'NAME': os.getenv('DB_NAME', 'event_postgres'),
+        'USER': os.getenv('DB_USER', 'event_postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'event_postgres'),
+        'HOST': os.getenv('DB_HOST', 'event_management_db'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
