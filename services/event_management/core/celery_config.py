40958d55bcac6885d21c28a0a0cf206fdd1f7c75
iman sadati <iman.3adati@gmail.com>
2025-07-12 22:59:46 +0330

    fix: Fix some errors in queue issues.

--- services/event_management/core/celery_config.py
+++ services/event_management/core/celery_config.py
@@ -1,6 +1,6 @@
 from celery import Celery
 import os
-from kombu import Queue
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
@@ -10,12 +10,11 @@ app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 app.conf.task_acks_late = True
 app.conf.task_default_priority = 5
 
-app.conf.task_routes = (
-    Queue(
-        'event_publish_queue',
-        routing_key='event_publish_queue',
-        queue_arguments={'x-max-priority': 10}
-    )
-)
+app.conf.task_routes = {
+    'apps.events.tasks': {
+        'queue': 'publish_event_queue', 'routing_key': 'publish_event_queue'
+    }
+}
 
+app.conf.task_default_queue = 'publish_event_queue'
 app.autodiscover_tasks(['apps.events'])
