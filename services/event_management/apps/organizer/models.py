87232fe2a12c4530773877d1880aeb4a95e935fc
iman sadati <iman.3adati@gmail.com>
2025-07-20 15:51:57 +0330

    feat: Add organizer model.

--- services/event_management/apps/organizer/models.py
+++ services/event_management/apps/organizer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Organizer(models.Model):
+    TYPE_CHOICES = [
+        ('company', 'Company'),
+        ('individual', 'Individual'),
+        ('organization', 'Organization'),
+    ]
+
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.CharField(
+        max_length=16, choices=TYPE_CHOICES, default='individual')
+    logo = models.ImageField(upload_to='images/organizer/', blank=True)
+    website = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.name
