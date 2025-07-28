769df1e14ff79df9a91d7fdd2eeac6990e781569
iman sadati <iman.3adati@gmail.com>
2025-07-28 14:58:27 +0330

    feat: Add venue update api.

--- services/event_management/apps/venue/apis.py
+++ services/event_management/apps/venue/apis.py
@@ -6,8 +6,9 @@ from rest_framework.viewsets import ViewSet
 from .selectors import venue_list, venue_get
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .services import venue_create
+from .services import venue_create, venue_update
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
 
 class VenueListApi(APIView):
@@ -58,6 +59,7 @@ class VenueCreateApi(APIView):
         class Meta:
             model = Venue
             exclude = ['created_at', 'updated_at']
+            extra_kwargs = {'venue_type': {'required': True}}
 
     def post(self, reqeust: HttpRequest):
         serializer = self.InputVenueSerializer(data=reqeust.data)
@@ -69,6 +71,43 @@ class VenueCreateApi(APIView):
         return Response(data, status=status.HTTP_201_CREATED)
 
 
+class VenueUpdateApi(APIView):
+    class InputVenueSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Venue
+            fields = ['name', 'address', 'city',
+                      'capacity']  # add more if needed
+
+        # check the user does not enter additional fields
+        def validate(self, data):
+            extra_fields = set(self.initial_data.keys()) - \
+                set(self.fields.keys())
+            if extra_fields:
+                raise ValidationError(
+                    {"extra_fields": f"Unexpected fields: {', '.join(extra_fields)}"})
+            return data
+
+    def put(self, request: HttpRequest, pk):
+        return self.update_venue(request, pk)
+
+    def patch(self, request: HttpRequest, pk):
+        return self.update_venue(request, pk)
+
+    def update_venue(self, request: HttpRequest, pk):
+        serializer = self.InputVenueSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        venue = venue_get(pk)
+        try:
+            updated_venue = venue_update(
+                venue=venue, data=serializer.validated_data)
+            return Response(VenueDetailApi.OutputVenueSerializer(updated_venue).data, status=status.HTTP_200_OK)
+        except Exception as e:
+            if e.get_codes() == ['no_content']:
+                return Response({'detail': 'No changes detected. venue data remains the same.'}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'errors': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class VenueGetwayApiViewset(ViewSet):
     def list(self, request: HttpRequest):
         return VenueListApi.as_view()(request._request)
@@ -78,3 +117,9 @@ class VenueGetwayApiViewset(ViewSet):
 
     def create(self, request: HttpRequest):
         return VenueCreateApi.as_view()(request._request)
+
+    def update(self, request: HttpRequest, pk=None):
+        return VenueUpdateApi.as_view()(request._request, pk=pk)
+
+    def partial_update(self, request: HttpRequest, pk=None):
+        return VenueUpdateApi.as_view()(request._request, pk=pk)
--- services/event_management/apps/venue/services.py
+++ services/event_management/apps/venue/services.py
@@ -1,5 +1,7 @@
 from django.db import transaction
 from .models import Venue
+from shared_utils.update_model import model_update
+from rest_framework.exceptions import ValidationError
 
 
 @transaction.atomic()
@@ -7,3 +9,20 @@ def venue_create(**kwargs):
     venue = Venue.objects.create(**kwargs)
 
     return venue
+
+
+def venue_update(*, venue: Venue, data):
+    non_side_effect_fields = [
+        'name',
+        'address',
+        'city',
+        'capacity'
+    ]
+
+    try:
+        updated_venue = model_update(
+            instance=venue, fields=non_side_effect_fields, data=data
+        )
+        return updated_venue
+    except ValidationError as e:
+        raise e
