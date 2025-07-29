2e95038397349d7725d2cd7c5c62133ac7ef9d64
iman sadati <iman.3adati@gmail.com>
2025-07-29 17:40:14 +0330

    feat: Add speaker update api.

--- services/event_management/apps/speaker/apis.py
+++ services/event_management/apps/speaker/apis.py
@@ -7,7 +7,8 @@ from rest_framework import serializers
 from .selectors import speaker_list, speaker_get
 from rest_framework import status
 from rest_framework.response import Response
-from .services import speaker_create
+from .services import speaker_create, speaker_update
+from rest_framework.exceptions import ValidationError
 
 
 class SpeakerListApi(APIView):
@@ -70,6 +71,43 @@ class SpeakerCreateApi(APIView):
         return Response(data, status=status.HTTP_201_CREATED)
 
 
+class SpeakerUpdateApi(APIView):
+    class InputSpeakerSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Speaker
+            fields = ['name', 'bio', 'status']  # add more if needed
+
+        # check the user does not enter additional fields
+        def validate(self, data):
+            extra_fields = set(self.initial_data.keys()) - \
+                set(self.fields.keys())
+            if extra_fields:
+                raise ValidationError(
+                    {"extra_fields": f"Unexpected fields: {', '.join(extra_fields)}"})
+            return data
+
+    def put(self, request: HttpRequest, pk):
+        return self.update_speaker(request, pk)
+
+    def patch(self, request: HttpRequest, pk):
+        return self.update_speaker(request, pk)
+
+    def update_speaker(self, request: HttpRequest, pk):
+        serializer = self.InputSpeakerSerializer(
+            data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        speaker = speaker_get(pk)
+        try:
+            updated_speaker = speaker_update(
+                speaker=speaker, data=serializer.validated_data)
+            return Response(SpeakerDetailApi.OutputSpeakerSerializer(updated_speaker).data, status=status.HTTP_200_OK)
+        except Exception as e:
+            if e.get_codes() == ['no_content']:
+                return Response({'detail': 'No changes detected. Speaker data remains the same.'}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'errors': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
 # To make endpoints RestFull
 class SpeakerGetwayApiViewset(ViewSet):
     def list(self, request: HttpRequest):
@@ -80,3 +118,9 @@ class SpeakerGetwayApiViewset(ViewSet):
 
     def create(self, request: HttpRequest):
         return SpeakerCreateApi.as_view()(request._request)
+
+    def update(self, request: HttpRequest, pk=None):
+        return SpeakerUpdateApi.as_view()(request._request, pk=pk)
+
+    def partial_update(self, request: HttpRequest, pk=None):
+        return SpeakerUpdateApi.as_view()(request._request, pk=pk)
--- services/event_management/apps/speaker/services.py
+++ services/event_management/apps/speaker/services.py
@@ -1,5 +1,7 @@
 from .models import Speaker
 from django.db import transaction
+from shared_utils.update_model import model_update
+from rest_framework.exceptions import ValidationError
 
 
 @transaction.atomic()
@@ -7,3 +9,19 @@ def speaker_create(**kwargs):
     speaker = Speaker.objects.create(**kwargs)
 
     return speaker
+
+
+def speaker_update(*, speaker: Speaker, data):
+    non_side_effect_fields = [
+        'name',
+        'bio',
+        'status',
+    ]
+
+    try:
+        updated_speaker = model_update(
+            instance=speaker, fields=non_side_effect_fields, data=data
+        )
+        return updated_speaker
+    except ValidationError as e:
+        raise e
