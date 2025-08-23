65dad27529daf87802ece7a27c5492d28fca834e
iman sadati <iman.3adati@gmail.com>
2025-08-23 20:17:56 +0330

    feat: Complete grpc client side in event app.

--- services/event_management/grpc_service/client/client.py
+++ services/event_management/grpc_service/client/client.py
@@ -1,9 +1,11 @@
+import grpc
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from grpc_service.client.generated import user_pb2
+from grpc_service.client.generated.user_pb2_grpc import UserServiceStub
 from grpc_service.client.generated import notification_pb2
 from grpc_service.client.generated.notification_pb2_grpc import NotificationServiceStub
-import grpc
-from grpc_service.client.generated.user_pb2_grpc import UserServiceStub
-from grpc_service.client.generated import user_pb2
-from rest_framework.exceptions import ValidationError
+from grpc_service.client.generated import user_with_role_pb2
+from grpc_service.client.generated.user_with_role_pb2_grpc import UserServiceViaRoleStub
 
 
 def validate_user_exists(user_id):
@@ -31,3 +33,43 @@ def send_email_via_rpc(recipient, subject, body):
             body=body
         ))
         return response.success
+
+
+class RemoteUser:
+    def __init__(self, id, email, role):
+        self.id = id
+        self.email = email
+        self.role = role
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def __str__(self):
+        return self.email
+
+
+class RemoteUserClient:
+    """ Validate any type of user (admin, staff, attendee) with role. """
+
+    @staticmethod
+    def get_user(user_id, role):
+        try:
+            with grpc.insecure_channel('user_management:50052') as channel:
+                stub = UserServiceViaRoleStub(channel)
+                response = stub.GetUserViaRole(user_with_role_pb2.GetUserViaRoleRequest(
+                    id=str(user_id),
+                    role=role
+                ))
+
+            return RemoteUser(id=response.id, email=response.email, role=response.role)
+            # return response
+
+        except grpc.RpcError as e:
+            if e.code() == grpc.StatusCode.NOT_FOUND:
+                return None
+            raise AuthenticationFailed(f"gRPC error: {e.details()}")
